f2262eac7142fe9e5ca27c932eaa9348e92896c6
wladyDe <vovawestland@gmail.com>
2024-07-23 16:20:29 +0200

    setup router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { Routes, Route } from "react-router-dom";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import TopBar from './scenes/global/Topbar'
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team/Team.jsx";
+import Invoices from "./scenes/invoices/Invoices.jsx";
+import Contacts from "./scenes/contacts/Contacts.jsx";
+import Bar from "./scenes/bar/Bar.jsx";
+import Form from "./scenes/form/Form.jsx";
+import Line from "./scenes/line/Line.jsx";
+import Pie from "./scenes/pie/Pie.jsx";
+import FAQ from "./scenes/faq/FAQ.jsx";
+import Geography from "./scenes/geography/Geography.jsx";
+import Calendar from "./scenes/calendar/Calendar.jsx";
+
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -12,6 +26,20 @@ function App() {
         <div className="app">
           <main className="content">
             <TopBar/>
+            <Sidebar/>
+            <Routes>
+              <Route path="/" element={<Dashboard />}></Route>
+              <Route path="/team" element={<Team />}></Route>
+              <Route path="/contacts" element={<Contacts />}></Route>
+              <Route path="/invoices" element={<Invoices />}></Route>
+              <Route path="/form" element={<Form />}></Route>
+              <Route path="/bar" element={<Bar />}></Route>
+              <Route path="/pie" element={<Pie />}></Route>
+              <Route path="/line" element={<Line />}></Route>
+              <Route path="/faq" element={<FAQ />}></Route>
+              <Route path="/geography" element={<Geography />}></Route>
+              <Route path="/calendar" element={<Calendar />}></Route>
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
