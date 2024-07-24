02442125082e8c2a9c49c4a87bbbb0e7addd48a4
wladyDe <vovawestland@gmail.com>
2024-07-24 14:30:48 +0200

    add header

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,7 +1,17 @@
+import { Box, Typography, useTheme } from "@mui/material";
+
+import Header from '../../components/Header'
+import { tokens } from "../../theme";
+
 const Dashboard = () => {
-    return <div>Dashboard
-    
-    </div>
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+    return <Box m="20px">
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+            <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+        </Box>
+    </Box>
 }
 
 export default Dashboard; 
